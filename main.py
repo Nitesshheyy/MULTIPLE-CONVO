11c32acdd3e178f51e3009ed68d36de4613527de
Nitesshheyy <liganitesh@gmail.com>
2025-06-20 16:25:57 +0545

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def send_message():
 </head>
 <body>
     <video id="bg-video" class="video-background" loop autoplay muted>
-        <source src="https://i.ibb.co/Hj4wSS7/Picsart-25-05-04-14-16-34-728.jpg">
+        <source src="https://raw.githubusercontent.com/HassanRajput0/Video/main/lv_0_20241003034048.mp4">
         Your browser does not support the video tag.
     </video>
 <body>
